3c821f70a9d8cf913a7789fc04480e8c4ea2cb15
X-CCS <CCS695146667@163.com>
2021-03-16 13:59:39 +0000

    updata

--- zhtts/tts.py
+++ zhtts/tts.py
@@ -5,8 +5,9 @@ import tensorflow as tf
 #import tflite_runtime.interpreter as tflite
 from scipy.io import wavfile
 import re
-
-from .tensorflow_tts.processor import BakerProcessor
+import sys
+sys.path.append("/home/project/zhtts/")
+from tensorflow_tts.processor import BakerProcessor # baker.py
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 ASSET_DIR = Path(__file__).parent / "asset"
@@ -35,12 +36,15 @@ class TTS():
             data_dir=None, loaded_mapper_path=ASSET_DIR / "baker_mapper.json")
         self.text2mel_name = text2mel_name
         if text2mel_name == "FASTSPEECH2":
-            self.acoustic = tf.lite.Interpreter(model_path=str(ASSET_DIR / 'fastspeech2_quan.tflite'))
+            # fastspeech2_quan.tflite
+            self.acoustic = tf.lite.Interpreter(model_path=str(ASSET_DIR / 'fastspeech2_test_quan.tflite'))
         elif text2mel_name == "TACOTRON":
+            # tacotron2_quan.tflite
             self.acoustic = tf.lite.Interpreter(model_path=str(ASSET_DIR / 'tacotron2_quan.tflite'))
         else:
             raise ValueError(f"unsported text2mel_name: {text2mel_name}")
-        self.vocoder = tf.lite.Interpreter(model_path=str(ASSET_DIR / 'mb_melgan.tflite'))
+        # mb_melgan.tflite
+        self.vocoder = tf.lite.Interpreter(model_path=str(ASSET_DIR / 'mb_melgan_test.tflite'))
 
     def prepare_input(self, input_ids):
         input_ids = np.expand_dims(np.array(input_ids, np.int32), 0)
@@ -56,10 +60,10 @@ class TTS():
                     np.array([1.0], np.float32),)
 
     def text2mel(self, input_text):
-        input_details = self.acoustic.get_input_details()
+        input_details = self.acoustic.get_input_details() 
         output_details = self.acoustic.get_output_details()
         input_ids = self.processor.text_to_sequence(input_text, inference=True)
-
+        
         self.acoustic.resize_tensor_input(
             input_details[0]['index'], [1, len(input_ids)])
         self.acoustic.allocate_tensors()
@@ -86,7 +90,7 @@ class TTS():
 
         Args:
             text (str)
-            sil_time (float): silence duration between two wav
+            sil_time (float): silence duration between two wav 两个wav之间的沉默时间
         Returns:
             ndarray: audio
         """
@@ -98,13 +102,24 @@ class TTS():
             print(f"frontend info: {self.frontend(text)}")
             # print(self.processor.text_to_sequence(text, inference=True))
             mel = self.text2mel(text)
+            # np.save("mel_tacotron.npy",mel)
+            # np.save("mel_fastseppch2.npy",mel)
+            # print("mel内容:",mel)
+            # print("mel类型:",type(mel))
+            # print("mel维度:",mel.shape) # fastspeech2(1, 259, 80) tacotorn2(1, 290, 80)
+
             audio = self.mel2audio(mel)
+            np.save("audio_tacotron.npy",audio)
+            # np.save("audio_fastseppch2.npy",audio)
+            print("audio内容:",audio)
+            print("audio类型:",type(audio))
+            print("audio维度:",audio.shape) # fastspeech2(77700,) tacotorn2(85800,)
             if self.text2mel_name == "TACOTRON":
                 audio = audio[:-2048]  # tacotron will generate noise at the end
             audios.append(audio)
             if i < len(texts)-1:
                 audios.append(silence)
-        return np.concatenate(audios)
+        return np.concatenate(audios) # 数组拼接函数
 
     def frontend(self, text):
         """ return normalize_text, phoneme_seq for debug
@@ -125,4 +140,14 @@ class TTS():
         
         wavfile.write(wavpath, self.sample_rate, audio)
         print(f"Save wav to {wavpath}")
-        
+
+if __name__ == "__main__": 
+    tts = TTS()
+    # tts = TTS(text2mel_name="TACOTRON") # tacotron2
+    tts = TTS(text2mel_name="FASTSPEECH2") # FASTSPEECH2
+    # text = "2020年，这是一个开源的端到端中文语音合成系统"
+    # res = tts.frontend(text)
+    # tts.synthesis(text)
+    tts.text2wav(text,"./test.wav")
+    # print(res)
+    print("done")
